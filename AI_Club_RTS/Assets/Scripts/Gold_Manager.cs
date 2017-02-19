afe9146a8fadb96e53bc05cbf26df0c38cf71012
Paul DeSimone <pjd3949@rit.edu>
2017-02-19 14:32:56 -0500

    UI Created and Implemented into Canvas

---
+++ AI_Club_RTS/Assets/Scripts/Gold_Manager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gold_Manager : MonoBehaviour {
+
+	public int currentGold;
+	const int second = 1;
+	float counter;
+
+	void Start () {
+		counter = 0;
+		currentGold = 0;
+	}
+	
+	void Update () {
+		UpdateGold ();
+	}
+
+	// Increments a players gold each second
+	void UpdateGold() {
+		counter += Time.deltaTime;
+		if (counter >= second) {
+			currentGold++;
+			counter = 0;
+		}
+	}
+}
---
+++ AI_Club_RTS/Assets/Scripts/UI_Manager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Manager : MonoBehaviour {
+
+	GameObject sceneManager;
+	Gold_Manager gold_Manager;
+
+	Text gold;
+
+
+	void Start () {
+		sceneManager = GameObject.Find ("Scene_Manager");
+		gold = GameObject.Find("Gold_Text").GetComponent<Text> ();
+		gold_Manager = sceneManager.GetComponent<Gold_Manager> ();
+		Debug.Log (sceneManager.name);
+		Debug.Log (gold_Manager.currentGold);
+	}
+
+	void Update () {
+		gold.text = "" +gold_Manager.currentGold;
+	}
+}
