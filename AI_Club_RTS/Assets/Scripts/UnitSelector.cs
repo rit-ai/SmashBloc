e472ebe8d927cbc4a369c1237b62d59cf748994a
Tory-PC\Tory <tjl1080@rit.edu>
2017-02-25 14:41:44 -0500

    code should work now with the selectableunit class and added select multiple units

--- AI_Club_RTS/Assets/Scripts/UnitSelector.cs
+++ AI_Club_RTS/Assets/Scripts/UnitSelector.cs
@@ -5,48 +5,52 @@ using UnityEngine;
 public class UnitSelector : MonoBehaviour {
 
 	bool isSelecting = false;
+
+	List<SelectableUnit> selectedUnits;  // List of current selected units. "Selectable Unit" can be changed to any class that you want to select
 	Vector3 mousePos;
 //	List<SelectableUnit> selectedUnits;
 
 	// Use this for initialization
 	void Start () {
-		
+		selectedUnits = new List<SelectableUnit>();
 	}
+		
+	void Update () {
 
-//	void Update () {
-//		if(selectedUnits != null) {
-//			//Debug.Log(selectedUnits.Count.ToString());
-//		}
-//		// Saves the mouse location when left mouse is down
-//		if(Input.GetMouseButtonDown(0)) {
-//			isSelecting = true;
-//			mousePos = Input.mousePosition;
-//		}
-//		// Ends selection when left mouse button is released
-//
-//		// If we let go of the left mouse button, finish selection
-//		if(Input.GetMouseButtonUp(0)) {
-//			// Gets all the units that can be selected 
-//			selectedUnits = new List<SelectableUnit>();
-//			foreach(var s in FindObjectsOfType<SelectableUnit>()) {
-//				if(IsWithinSelectionBounds(s.gameObject)) {
-//					selectedUnits.Add(s);
-//				}
-//			}
-//			isSelecting = false;
-//		}
-//	}
+		// Saves the mouse location when left mouse is down
+		if(Input.GetMouseButtonDown(0)) {
+			isSelecting = true;
+			mousePos = Input.mousePosition;
+		}
+
+		// Ends selection when left mouse button is released
+		if(Input.GetMouseButtonUp(0)) {
+			// Gets all the units that can be selected 
+			if(! Input.GetKey(KeyCode.LeftShift)) {
+				Debug.Log("new list, components deselected");
+				selectedUnits = new List<SelectableUnit>();
+			}
+			foreach(SelectableUnit s in FindObjectsOfType<SelectableUnit>()) {
+				if(IsWithinSelectionBounds(s.gameObject) && !selectedUnits.Contains(s)) {
+					selectedUnits.Add(s);
+					Debug.Log("added");
+				}
+			}
+			isSelecting = false;
+		}
+	}
 
 	void OnGUI() {
 		// Calls functions to draw the selection box
 		if(isSelecting) {
-			var rect = Utils.GetScreenRect(mousePos, Input.mousePosition);
+			Rect rect = Utils.GetScreenRect(mousePos, Input.mousePosition);
 			Utils.DrawScreenRect(rect, new Color(0.8f, 0.8f, 0.95f, 0.25f));
 			Utils.DrawScreenRectBorder(rect, 2, new Color(.8f, .0f, 0.95f));
 		}
 
 	}
 
+	// Checks to see if a given object is within the area being selected
 	public bool IsWithinSelectionBounds( GameObject gameObject )
 	{
 		if( !isSelecting ){
